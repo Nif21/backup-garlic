33160f9539b44538b2ccc9fbbb02751cc378a271
Fauzan Ramadhan <fauzanramadhan59@gmail.com>
2021-10-06 00:37:29 +0700

    add legend kesesuain lahan

--- src/components/EsriMap.js
+++ src/components/EsriMap.js
@@ -29,35 +29,7 @@ function EsriMap() {
       });
  
 
-      const AllLayer = new FeatureLayer({
-        copyright: "BELIIN MEKDI DONG",
-        title: "All Layers",
-        fields: [
-          {
-            name: "ObjectID",
-            alias: "ObjectID",
-            type: "oid"
-          },
-          {
-            name: "spt",
-            alias: "spt",
-            type: "string"
-          },
-          {
-            name: "proporsi",
-            alias: "proporsi",
-            type: "string"
-          }
-        ],
-        objectIdField: "ObjectID",
-        geometryType: "polygon",
-        source: [],
-        popupTemplate: {
-          title: "{spt}",
-          proporsi: "{proporsi}"
-        }
-      });
-      map.add(AllLayer);
+   
 
       const legend = new Legend({
         view: view
@@ -65,19 +37,19 @@ function EsriMap() {
       view.ui.add(legend, "bottom-left");
       
       (async () => {
-        const data = await fetch("http://localhost:8000/v1/show")
+        const data = await fetch("http://40bb-180-245-81-100.ngrok.io/v1/show")
         const dataJSON = await data.json();
  
         const simpleFillSymbol = {
           type: "simple-fill",
-          color: [227, 139, 79, 0.8],  // Orange, opacity 80%
+          color: [255, 0, 0, 0.8],  // Orange, opacity 80%
           outline: {
             color: [255, 255, 255],
             width: 1
           }
         };
         let polygon ;
-        const graphics = dataJSON.map((v)=>{
+        const graphicsNormal = dataJSON.map((v)=>{
           polygon = {
             type: "polygon",
             rings: v.geom.coordinates[0][0]
@@ -102,11 +74,273 @@ function EsriMap() {
             geometry: polygon,
             symbol: simpleFillSymbol,
             attributes: v,
-            outFields: ["klasifikasitanah","CITY_JUR","X_STREET","PARKING","ELEV_FT"],
             popupTemplate: popupTrailheads
           });
         })
-        AllLayer.applyEdits({addFeatures: graphics})
+
+        const graphicsS3 = dataJSON.map((v)=>{
+          if(v.kelasfaktorlandscape == "S3"){
+              polygon = {
+            type: "polygon",
+            rings: v.geom.coordinates[0][0]
+          };
+          }
+        
+          const popupTrailheads = {
+            "title": "No SPT : " + v.spt.toString() + " (Proporsi " + v.proporsi + " )",
+            "content":  "<h1><b>Kelas Faktor Landscape:</b> " + v.kelasfaktorlandscape + "</h1>" + 
+                        "<br><b>Bahan Induk:</b> " + v.bahaninduk + 
+                        "<br><b>Kedalam Mineral Tanah : </b>"+ v.kedalamanmineraltanah + 
+                        "<br><b>Drainase: </b> " + v.drainase +
+                        "<br><b>Tekstur Tanah:</b> " + v.teksturtanah + 
+                        "<br><b>Kemasaman Tanah: </b> " + v.kemasamantanah +
+                        "<br><b>Kapasitas Tukar Kation: </b> " + v.kapasitastukarkation +
+                        "<br><b>Kejenuhan Basa: </b> " + v.kejenuhanbasa +
+                        "<br><b>Land Form: </b> " + v.landform +
+                        "<br><b>Bahan Induk: </b> " + v.bahaninduk +
+                        "<br><b>Relief: </b> " + v.relief +
+                        "<br><b>Luas: </b> " + v.luas +
+                        "<br><b>Persentase Luas: </b> " + v.persentaseluas
+          }
+          return new Graphic({
+            geometry: polygon,
+            symbol: simpleFillSymbol,
+            attributes: v,
+            popupTemplate: popupTrailheads
+          });
+        })
+
+        const graphicsS2 = dataJSON.map((v)=>{
+          if(v.kelasfaktorlandscape == "S2"){
+            polygon = {
+              type: "polygon",
+              rings: v.geom.coordinates[0][0]
+            };
+          }      
+          const popupTrailheads = {
+            "title": "No SPT : " + v.spt.toString() + " (Proporsi " + v.proporsi + " )",
+            "content":  "<h1><b>Kelas Faktor Landscape:</b> " + v.kelasfaktorlandscape + "</h1>" + 
+                        "<br><b>Bahan Induk:</b> " + v.bahaninduk + 
+                        "<br><b>Kedalam Mineral Tanah : </b>"+ v.kedalamanmineraltanah + 
+                        "<br><b>Drainase: </b> " + v.drainase +
+                        "<br><b>Tekstur Tanah:</b> " + v.teksturtanah + 
+                        "<br><b>Kemasaman Tanah: </b> " + v.kemasamantanah +
+                        "<br><b>Kapasitas Tukar Kation: </b> " + v.kapasitastukarkation +
+                        "<br><b>Kejenuhan Basa: </b> " + v.kejenuhanbasa +
+                        "<br><b>Land Form: </b> " + v.landform +
+                        "<br><b>Bahan Induk: </b> " + v.bahaninduk +
+                        "<br><b>Relief: </b> " + v.relief +
+                        "<br><b>Luas: </b> " + v.luas +
+                        "<br><b>Persentase Luas: </b> " + v.persentaseluas
+          }
+          return new Graphic({
+            geometry: polygon,
+            symbol: simpleFillSymbol,
+            attributes: v,
+            popupTemplate: popupTrailheads
+          });
+        })
+
+        const graphicsS1 = dataJSON.map((v)=>{
+          if(v.kelasfaktorlandscape == "S1"){
+              polygon = {
+            type: "polygon",
+            rings: v.geom.coordinates[0][0]
+          };
+          }
+        
+          const popupTrailheads = {
+            "title": "No SPT : " + v.spt.toString() + " (Proporsi " + v.proporsi + " )",
+            "content":  "<h1><b>Kelas Faktor Landscape:</b> " + v.kelasfaktorlandscape + "</h1>" + 
+                        "<br><b>Bahan Induk:</b> " + v.bahaninduk + 
+                        "<br><b>Kedalam Mineral Tanah : </b>"+ v.kedalamanmineraltanah + 
+                        "<br><b>Drainase: </b> " + v.drainase +
+                        "<br><b>Tekstur Tanah:</b> " + v.teksturtanah + 
+                        "<br><b>Kemasaman Tanah: </b> " + v.kemasamantanah +
+                        "<br><b>Kapasitas Tukar Kation: </b> " + v.kapasitastukarkation +
+                        "<br><b>Kejenuhan Basa: </b> " + v.kejenuhanbasa +
+                        "<br><b>Land Form: </b> " + v.landform +
+                        "<br><b>Bahan Induk: </b> " + v.bahaninduk +
+                        "<br><b>Relief: </b> " + v.relief +
+                        "<br><b>Luas: </b> " + v.luas +
+                        "<br><b>Persentase Luas: </b> " + v.persentaseluas
+          }
+          return new Graphic({
+            geometry: polygon,
+            symbol: simpleFillSymbol,
+            attributes: v,
+            popupTemplate: popupTrailheads
+          });
+        })
+
+        const normalLayer = new FeatureLayer({
+          fields: [
+            {
+              name: "ObjectID",
+              alias: "ObjectID",
+              type: "oid"
+            },
+            {
+              name: "spt",
+              alias: "spt",
+              type: "string"
+            },
+            {
+              name: "proporsi",
+              alias: "proporsi",
+              type: "string"
+            }
+          ],
+          objectIdField: "ObjectID",
+          geometryType: "polygon",
+          source: graphicsNormal,
+          renderer: {
+            type: "simple",
+            "symbol": {
+              "color": "#FF4500",
+              "type": "simple-fill",
+              "style": "solid",
+              "outline": {
+                color: [255, 255, 255],
+                width: 1
+
+              }
+            },
+            "label": "Normal"
+          },
+          popupTemplate: {
+            title: "{spt}",
+            proporsi: "{proporsi}"
+          }
+        });
+      
+
+        const s1Layer = new FeatureLayer({
+          title: "Layer Kesesuain Lahan",
+          fields: [
+            {
+              name: "ObjectID",
+              alias: "ObjectID",
+              type: "oid"
+            },
+            {
+              name: "spt",
+              alias: "spt",
+              type: "string"
+            },
+            {
+              name: "proporsi",
+              alias: "proporsi",
+              type: "string"
+            }
+          ],
+          objectIdField: "ObjectID",
+          geometryType: "polygon",
+          source: graphicsS1,
+          renderer: {
+            type: "simple",
+            "symbol": {
+              "color": "#F0E68C",
+              "type": "simple-fill",
+              "style": "solid",
+              "outline": {
+                color: [255, 255, 255],
+                width: 1
+
+              }
+            },
+            "label": "S1"
+          },
+          popupTemplate: {
+            title: "{spt}",
+            proporsi: "{proporsi}"
+          }
+        });
+      
+        const s3Layer = new FeatureLayer({
+          fields: [
+            {
+              name: "ObjectID",
+              alias: "ObjectID",
+              type: "oid"
+            },
+            {
+              name: "spt",
+              alias: "spt",
+              type: "string"
+            },
+            {
+              name: "proporsi",
+              alias: "proporsi",
+              type: "string"
+            }
+          ],
+          objectIdField: "ObjectID",
+          geometryType: "polygon",
+          source: graphicsS3,
+          renderer: {
+            type: "simple",
+            "symbol": {
+              "color": "#800000",
+              "type": "simple-fill",
+              "style": "solid",
+              "outline": {
+                color: [255, 255, 255],
+                width: 1
+
+              }
+            },
+            "label": "S3"
+          },
+          popupTemplate: {
+            title: "{spt}",
+            proporsi: "{proporsi}"
+          }
+        });
+        const s2Layer = new FeatureLayer({
+          fields: [
+            {
+              name: "ObjectID",
+              alias: "ObjectID",
+              type: "oid"
+            },
+            {
+              name: "spt",
+              alias: "spt",
+              type: "string"
+            },
+            {
+              name: "proporsi",
+              alias: "proporsi",
+              type: "string"
+            }
+          ],
+          objectIdField: "ObjectID",
+          geometryType: "polygon",
+          source: graphicsS2,
+          renderer: {
+            type: "simple",
+            "symbol": {
+              "color": "#00FF7F",
+              "type": "simple-fill",
+              "style": "solid",
+              "outline": {
+                color: [255, 255, 255],
+                width: 1
+
+              }
+            },
+            "label": "S2"
+          },
+          popupTemplate: {
+            title: "{spt}",
+            proporsi: "{proporsi}"
+          }
+        });
+        map.add(normalLayer);
+        map.add(s3Layer);
+        map.add(s2Layer);
+        map.add(s1Layer);
       })()
     }
   }, []);
