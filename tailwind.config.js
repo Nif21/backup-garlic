455e5f2141a90246ae852342f3d1c3b78273261c
Fauzan Ramadhan <fauzanramadhan59@gmail.com>
2022-01-05 19:31:37 +0700

    fix:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,30 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./src/pages/**/*.js", "./src/components/**/*.js"],
+
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         sans: ["IBM Plex Sans", "ui-sans-serif", "system-ui"],
-         },
-      colors:{
-        "coco":{
-          "normal":"#948E68"
+      },
+      colors: {
+        coco: {
+          normal: "#948E68",
         },
-        "primary":{
-          "dark":"#125C13",
-          "normal": "#3E7C17",
-          "light": "#F4A442",
-          "white": "#FEFDF9",
-          "lesswhite": "#F0EEE7",
-          "coco": "#F4A442",
-          "darkcoco":"#E39A40"
-        }
-      
-      }   
+        primary: {
+          dark: "#125C13",
+          normal: "#3E7C17",
+          light: "#F4A442",
+          white: "#FEFDF9",
+          lesswhite: "#F0EEE7",
+          coco: "#F4A442",
+          darkcoco: "#E39A40",
+        },
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'), 
-  ],
-}
+  plugins: [require("daisyui")],
+};
