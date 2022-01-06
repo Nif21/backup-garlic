adf7b3a20c6545766fc7ff16359ea6fe218ea8be
Fauzan Ramadhan <fauzanramadhan59@gmail.com>
2022-01-06 12:00:26 +0700

    fix htmlfor

--- src/components/HeaderDrawer.js
+++ src/components/HeaderDrawer.js
@@ -6,7 +6,7 @@ export default function Header() {
       <div>
         <div className="text-2xl mx-5 mt-2  text-white">INA Agro-GARLIC</div>
         <div className="mx-5  text-white text-xs">
-          Agroecological Assessment of Land Suitability htmlFor Garlic
+          Agroecological Assessment of Land Suitability For Garlic
         </div>
       </div>
     </div>
