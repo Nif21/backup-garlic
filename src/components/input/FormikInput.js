695ae8218522b8b1fe5ddfc8112aee36a4a56b75
KillerQueen59 <fauzanramadhan59@gmail.com>
2022-03-17 08:18:02 -0700

    fix

--- src/components/input/FormikInput.js
+++ src/components/input/FormikInput.js
@@ -616,10 +616,10 @@ const FormikInput = () => {
             Submit
           </button>
         </div>
-        {showModal ? (
+        {showModal && (
           <>
-            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-              <div className="relative w-auto my-6 mx-auto max-w-6xl">
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-scroll fixed inset-0 z-50 outline-none focus:outline-none mt-16	">
+              <div className=" w-auto my-6 mx-auto max-w-6xl 	mt-auto">
                 {/*content*/}
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   {/*header*/}
@@ -951,7 +951,7 @@ const FormikInput = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="relative p-6 flex-auto">
+                  <div className="relative p-6 flex-auto text-black">
                     <p> Rekomendasi : </p>
                     <br />
                     {syaratTumbuh.karakteristikTanah.KelasDrainase == null ? (
@@ -1089,7 +1089,7 @@ const FormikInput = () => {
             </div>
             <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
           </>
-        ) : null}
+        ) }
       </form>
     </div>
   );
