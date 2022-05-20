2a8b03c49c85b21fc2f1a43a81db8839ae1a0da0
Fauzan Ramadhan <fauzanramadhan59@gmail.com>
2022-05-21 01:03:27 +0700

    remove jenis unit

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -177,17 +177,6 @@ const Register = () => {
                         </Field>
                       </dd>
                     </div>
-                    <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-6 sm:gap-4 sm:px-6">
-                      <dt className="text-sm font-medium text-gray-500">
-                        Jenis Unit IPB
-                      </dt>
-                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-5">
-                        <Field className={styles.field} as="select" name="tipe">
-                          <option value="ipb">Civitas IPB</option>
-                          <option value="nonipb">Umum</option>
-                        </Field>
-                      </dd>
-                    </div>
                     <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-6 sm:gap-4 sm:px-6">
                       <dt className="text-sm font-medium text-gray-500">
                         Password
